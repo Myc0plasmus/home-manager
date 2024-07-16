ff8949cceae0f693ff33339599eb61abf18af3f5
Myc0plasmus <jablonski.natan@gmail.com>
2024-07-16 19:07:47 +0200

    trying out overseer

--- nixvim/neovim.nix
+++ nixvim/neovim.nix
@@ -17,6 +17,7 @@ in
 	(getConfig "harpoon.nix")
 	(getConfig "lsp.nix")
 	(getConfig "which-key.nix")
+	(getConfig "overseer.nix")
 	# (getConfig "noice.nix")
 
   ];
---
+++ nixvim/plugSetup/overseer.nix
@@ -0,0 +1,16 @@
+{ config,inputs , pkgs, ... }:
+
+let
+	toLua = str: "lua << EOF\n${str}\nEOF\n";
+	toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+in
+{
+	programs.nixvim = {
+		extraPlugins = with pkgs.vimPlugins; [
+			{
+				plugin = overseer-nvim;
+				config = toLuaFile ./overseer_config.lua;
+			}
+		];
+	};
+}
