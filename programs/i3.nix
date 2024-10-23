bc0dba4feb9825e10b4931bd756fa4571c245527
Myc0plasmus <jablonski.natan@gmail.com>
2024-10-23 23:36:36 +0200

    another binding change - irritating

--- programs/i3.nix
+++ programs/i3.nix
@@ -163,7 +163,7 @@ in
 		"${modifier}+Ctrl+f" = "exec rofi -show filebrowser";
 
 		#Start window
-		"${modifier}+;" = "exec rofi -show window";
+		"${modifier}+n" = "exec rofi -show window";
 		
 		#Start ssh
 		"${modifier}+Ctrl+s" = "exec rofi -show ssh";
