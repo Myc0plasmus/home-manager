1c2344c3aa63175391da51700ae3c39fbe2e5245
Myc0plasmus <jablonski.natan@gmail.com>
2024-10-23 21:59:31 +0200

    Flake update and reverting the lsp fix - based on deprecated version

--- nixvim/plugSetup/lsp.nix
+++ nixvim/plugSetup/lsp.nix
@@ -13,9 +13,9 @@
 				# 	end
 				# '';
 				servers = {
-					ts-ls.enable = true;	
-					lua-ls.enable = true;	
-					rust-analyzer = {
+					ts_ls.enable = true;	
+					lua_ls.enable = true;	
+					rust_analyzer = {
 						enable = true;	
 						installCargo = true;
 						installRustc = true;
@@ -25,11 +25,11 @@
 					jsonls.enable = true;	
 					html.enable = true;	
 					cmake.enable = true;	
-					csharp-ls.enable = true;	
-					kotlin-language-server.enable = true;	
+					csharp_ls.enable = true;	
+					kotlin_language_server.enable = true;	
 					pyright.enable = true;	
 					# rnix.enable = true;	
-					r-language-server.enable = true;	
+					r_language_server.enable = true;	
 					
 				};
 				# keymaps.diagnostic = {
