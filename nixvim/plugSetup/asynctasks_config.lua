a3c2bb4a6531745a0e5389b288d0c41638b74630
Myc0plasmus <jablonski.natan@gmail.com>
2024-10-24 23:49:46 +0200

    testing vimscript dictionairy

--- nixvim/plugSetup/asynctasks_config.lua
+++ nixvim/plugSetup/asynctasks_config.lua
@@ -1,8 +1,9 @@
 vim.api.nvim_exec([[
 	function! s:my_toggleterm(opts)
-		echo a:opts
+		echo a:opts['VIM_ROOT']
+		echo a:opts['CMD']
 	endfunction
 
 	let g:asyncrun_runner = get(g:, 'asyncrun_runner', {})
-	let g:asyncrun_runner.test = function('s:my_toggleterm')
+	let g:asyncrun_runner.my_toggleterm = function('s:my_toggleterm')
 ]], false)
