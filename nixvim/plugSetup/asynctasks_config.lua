5f58bada382c7ffaf7d9a733db8e4967749a6fc7
Myc0plasmus <jablonski.natan@gmail.com>
2024-10-25 01:20:11 +0200

    Semi working toggleterm terminal parsing

--- nixvim/plugSetup/asynctasks_config.lua
+++ nixvim/plugSetup/asynctasks_config.lua
@@ -1,9 +1,17 @@
+-- Define a Lua function to accept the Vimscript dictionary
+local function my_toggleterm(opts)
+    -- Simulate running the command (e.g., with toggleterm)
+    require'toggleterm'.exec(string.format("cd %s && %s", opts.cwd, opts.cmd))
+end
+
+-- Register the function globally
+_G.my_toggleterm = my_toggleterm
+
 vim.api.nvim_exec([[
 	function! s:my_toggleterm(opts)
-		echo a:opts['VIM_ROOT']
-		echo a:opts['CMD']
+		call luaeval('my_toggleterm(_A)', a:opts)
 	endfunction
-
+	
 	let g:asyncrun_runner = get(g:, 'asyncrun_runner', {})
 	let g:asyncrun_runner.my_toggleterm = function('s:my_toggleterm')
 ]], false)
