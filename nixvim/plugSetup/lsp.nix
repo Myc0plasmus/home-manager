765693e232c16382e2db664c75eb988a0ebea620
Myc0plasmus <jablonski.natan@gmail.com>
2024-10-27 09:53:52 +0100

    resurrecting r language server

--- nixvim/plugSetup/lsp.nix
+++ nixvim/plugSetup/lsp.nix
@@ -31,7 +31,10 @@
 					# rnix.enable = true;	
 
 					# you have to define a package, it seems - I was lazy and I don't think I am going to be using R any time soon anyway
-					# r_language_server.enable = true;	
+					r_language_server = {
+						enable = true;
+						package = null;
+					};
 					
 				};
 				# keymaps.diagnostic = {
