c204e770753a5cec82585570087dc2df1e721f7b
Myc0plasmus <jablonski.natan@gmail.com>
2025-05-05 21:03:29 +0200

    changing for lightdm lock for now

--- scripts/lockscript.nix
+++ scripts/lockscript.nix
@@ -3,9 +3,9 @@
 pkgs.writeShellScriptBin "lockscript" ''
   # Lock screen displaying this image.
   # i3lock -i /etc/nixos/dotfiles/i3/lockscreen_3440x1440.png
-  export XAUTHORITY="$HOME/.Xauthority"
-  export DISPLAY=":0"
-  ${pkgs.betterlockscreen}/bin/betterlockscreen -l --off 1
+  # ${pkgs.betterlockscreen}/bin/betterlockscreen -l --off 1
+  dm-tool lock
+
 
   # Turn the screen off after a delay.
   # sleep 300 && pgrep i3lock && xset dpms force off
