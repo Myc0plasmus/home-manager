2df4a8d74879b3770c30c0d23b7d7cc1d955cdec
Myc0plasmus <jablonski.natan@gmail.com>
2025-05-07 20:44:39 +0200

    fixed screen locking

--- scripts/lockscript.nix
+++ scripts/lockscript.nix
@@ -3,8 +3,8 @@
 pkgs.writeShellScriptBin "lockscript" ''
   # Lock screen displaying this image.
   # i3lock -i /etc/nixos/dotfiles/i3/lockscreen_3440x1440.png
-  # ${pkgs.betterlockscreen}/bin/betterlockscreen -l --off 1
-  dm-tool lock
+  ${pkgs.betterlockscreen}/bin/betterlockscreen -l --off 5
+  # dm-tool lock
 
 
   # Turn the screen off after a delay.
