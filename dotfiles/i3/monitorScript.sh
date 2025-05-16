a798360249ac21e8cf6c5aa303fc03f7fbcdadaf
Myc0plasmus <jablonski.natan@gmail.com>
2025-05-16 22:24:22 +0200

    changed default monitor setup

--- dotfiles/i3/monitorScript.sh
+++ dotfiles/i3/monitorScript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if xrandr --query | grep "HDMI-1 connected"
 then
-    xrandr --output HDMI-1 --auto --above eDP-1
+    xrandr --output HDMI-1 --auto --right-of eDP-1
 else
     xrandr --output HDMI-1 --off
 fi
