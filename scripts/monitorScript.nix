c22942b492768a781add74d6d1bce1559c38fbc8
Myc0plasmus <jablonski.natan@gmail.com>
2025-10-02 00:27:51 +0200

    changing xrandr script

--- scripts/monitorScript.nix
+++ scripts/monitorScript.nix
@@ -7,9 +7,9 @@ pkgs.writeShellScriptBin "monitorScript" ''
     then
       ${pkgs.xorg.xrandr}/bin/xrandr --newmode "3440x1440_60.00r" 319.75  3440 3488 3520 3600  1440 1443 1453 1481 +hsync -vsync 
       ${pkgs.xorg.xrandr}/bin/xrandr --addmode HDMI-1 "3440x1440_60.00r"
-      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --mode "3440x1440_60.00r" --right-of eDP-1
+      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --mode "3440x1440_60.00r" --above eDP-1
     else
-      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --auto --right-of eDP-1
+      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --auto --above eDP-1
     fi
   else
   	${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --off
