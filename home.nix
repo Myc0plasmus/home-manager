088916fcfee7400703dddeb2218eb53ed8ee759e
Myc0plasmus <jablonski.natan@gmail.com>
2025-12-26 17:21:50 +0100

    trying out exporting nixvim directly

--- home.nix
+++ home.nix
@@ -47,7 +47,7 @@ in
     ./programs/rofi.nix
     ./programs/stylix.nix
     ./nixvim/neovim.nix
-    inputs.home-manager-config.inputs.nixvim.homeModules.nixvim
+    inputs.home-manager-config.homeModules.nixvim
   ];
   # The home.packages option allows you to install Nix packages into your
   # environment.
