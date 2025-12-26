49f115d2cb714c342c01bd28dbeac3bccc27f226
Myc0plasmus <jablonski.natan@gmail.com>
2025-12-26 16:54:44 +0100

    test with commit

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:/nixos/nixpkgs?ref=nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     polybar-spotify = {
       url = "github:PrayagS/polybar-spotify";
       flake = false;
@@ -24,58 +24,53 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix = {
-      url = "github:danth/stylix";
-    };
   };
 
   outputs =
     {
       nixpkgs,
-      home-manager,
-      polybar-spotify,
-      polybar-scripts,
-      polybar-calendar,
       nixvim,
-      stylix,
       ...
     }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      home = import ./home.nix;
-    in
     {
-      # lib.mkHome = {deviceName, username, ...}: {
-      #     home-manager.useGlobalPkgs = true;
-      #     home-manager.useUserPackages = true;
-      #     home-manager.extraSpecialArgs = {
-      #       inherit inputs;
-      #       home_username = username;
-      #       dev = deviceName;
-      #     };
-      #     home-manager.users."${username}" = home;
-      #
-      # };
-      homeConfigurations."${(import ./user.nix).home_username}" =
-        home-manager.lib.homeManagerConfiguration
-          {
-            inherit pkgs;
-
-            extraSpecialArgs = {
-              inherit inputs;
-              home_username = (import ./user.nix).home_username;
-              dev = (import ./user.nix).home_username;
-            };
-            # Specify your home configuration modules here, for example,
-            # the path to your home.nix.
-            modules = [
-              ./home.nix
-              stylix.homeModules.stylix
-            ];
-
-            # Optionally use extraSpecialArgs
-            # to pass through arguments to home.nix
-          };
+      inputs = inputs; 
     };
+    # let
+    #   system = "x86_64-linux";
+    #   pkgs = nixpkgs.legacyPackages.${system};
+    #   home = import ./home.nix;
+    # in
+    # {
+    #   # lib.mkHome = {deviceName, username, ...}: {
+    #   #     home-manager.useGlobalPkgs = true;
+    #   #     home-manager.useUserPackages = true;
+    #   #     home-manager.extraSpecialArgs = {
+    #   #       inherit inputs;
+    #   #       home_username = username;
+    #   #       dev = deviceName;
+    #   #     };
+    #   #     home-manager.users."${username}" = home;
+    #   #
+    #   # };
+    #   homeConfigurations."${(import ./user.nix).home_username}" =
+    #     home-manager.lib.homeManagerConfiguration
+    #       {
+    #         inherit pkgs;
+    #
+    #         extraSpecialArgs = {
+    #           inherit inputs;
+    #           home_username = (import ./user.nix).home_username;
+    #           dev = (import ./user.nix).home_username;
+    #         };
+    #         # Specify your home configuration modules here, for example,
+    #         # the path to your home.nix.
+    #         modules = [
+    #           ./home.nix
+    #           stylix.homeModules.stylix
+    #         ];
+    #
+    #         # Optionally use extraSpecialArgs
+    #         # to pass through arguments to home.nix
+    #       };
+    # };
 }
